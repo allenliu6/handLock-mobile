8d921558cfeff99d7502c79eac01dc2ff6959183
yonglun liu <allenliu6@yeah.net>
2017-03-29 23:18:34 +0800

    进行了界面的一些修正

--- handPassword.js
+++ handPassword.js
@@ -1,5 +1,7 @@
-//？？？？控制touchmove事件触发次数
-//lastPoint
+//控制touchmove事件触发次数   经过一些测试，暂时定为40ms一次
+//lastPoint  初始化
+//大量使用es6,需要浏览器支持度较高
+//endcallback   优雅弹窗
 
 
 'use strict'//移动端
@@ -24,8 +26,9 @@ Object.assign(touchLock.prototype, {
        this.record = []    //当前按序走过的所有圆的序号   record: [5,6,3]
        this.testStep = 0   //验证情况，0表示没设置密码  1表示设置过一次密码需要再确认  2表示设置成功密码  同时赋值localStorage.handPassword  不成功则重新回到0
        this.tempRecord = []    //临时存储圆的序号，设置重复验证时，验证密码时暂存与localStorage的数据
+       this.timer = true   //true表示当前可以进行touchmove回调函数，false则相反
        this.yellowMan = []
-       this.initYellowMan()
+       this.computeYellowMan()
 
         //页面初始化
        this.drawCircle()
@@ -33,16 +36,21 @@ Object.assign(touchLock.prototype, {
     },
     initDom(){
         let content = document.createElement('div'),
-            str = `<canvas id="canvas" width="${this.width}" height="${this.height}" style="margin-top: 20%;">
-                    <p>Your browserdoes not support the canvas element.</p> 
+            str = `<canvas id="canvas" width="${this.width}" height="${this.height}" style="margin-top: 15%;">
+                    <p>Your browser does not support the canvas element.</p> 
                 </canvas>
-                <h2 id="title" style="margin-bottom: 10%;">请输入手势密码</h2>
-                <form style="font-size:20px">
+                <h2 id="title" style="margin-bottom: 10%;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;">
+                    请输入手势密码
+                </h2>
+                <form style="font-size:20px;">
                     <input id="setRadio" name="type" type="radio" checked>设置密码<br>
                     <input id="testRadio" name="type" type="radio">验证密码
                 </form>`
 
-        content.setAttribute('style', `position: absolute;left: 0;right: 0;top: 0;bottom: 0;text-align: center;background-image:url("background2.jpeg"); background-position: center 0; background-repeat: no-repeat; background-attachment:fixed; background-size: cover;filter:alpha(opacity=60);-moz-opacity:0.6;opacity: 0.6;`)
+        content.setAttribute('style', `position: absolute;left: 0;right: 0;top: 0;bottom: 0;text-align: center;
+            color: #fff;
+            background-image:url("background2.jpeg"); background-position: center 0;
+            background-repeat: no-repeat; background-attachment:fixed; background-size: cover;`)
 
         content.innerHTML = str
         document.body.appendChild(content)
@@ -51,36 +59,37 @@ Object.assign(touchLock.prototype, {
     computeCircle(){
         let n = this.chooseType,// 画出n*n的矩阵 
             arr = [],
-            r = this.width / (4 * n);// 公式计算 半径和canvas的大小有关
+            r = this.width / (4 * n - 1);// 公式计算 半径和canvas的大小有关
 
         for (let i = 0 ; i < n ; i++) {
             for (let j = 0 ; j < n ; j++) {
                 arr.push({
-                    x: j * 4 * r + 2 * r,
-                    y: i * 4 * r + 2 * r
+                    x: j * 4 * r + 1.5 * r,
+                    y: i * 4 * r + 1.5 * r
                 });
             }
         }
 
         return [arr, r];
     },
-    drawCircle(){
-        for (let val of this.arr) {
-            this.ctx.strokeStyle = '#000';
-            this.ctx.lineWidth = 2;
-            this.ctx.beginPath();
-            this.ctx.arc(val.x, val.y, this.r, 0, Math.PI * 2);
-            this.ctx.stroke();
-        }
-    },
-    initYellowMan(){
+    computeYellowMan(){//初始化小黄人的序号
         let s = new Set()
         while(s.size !== 9){
             s.add(Math.ceil(Math.random() * 19))
         }
         this.yellowMan = [...s]
     },
-    drawPicture(index){//使经过的圆里面呈现图片  每次固定都是一张图片但需要不断刷新
+    drawCircle(){//将大圆绘制出来
+        for (let val of this.arr) {
+            this.ctx.strokeStyle = '#fff';
+            this.ctx.lineWidth = 3;
+            this.ctx.beginPath();
+            this.ctx.arc(val.x, val.y, this.r, 0, Math.PI * 2);
+            this.ctx.stroke();
+        }
+    },
+    //@Params当前圆的次序  使经过的圆里面呈现图片  每次固定都是一张图片但需要不断刷新
+    drawContainer(index){
         // 使经过的圆心形成小圆
         // this.ctx.fillStyle='#CFE6FF'
         // this.ctx.beginPath();
@@ -88,21 +97,21 @@ Object.assign(touchLock.prototype, {
         // this.ctx.fill();
         let pic = new Image(),
             val = this.arr[index]; 
-             
+
         pic.src =`./yellowMan/${this.yellowMan[index]}.png`;  
         this.ctx.drawImage(pic, val.x-this.r*2/3, val.y-this.r*2/3, this.r*1.5, this.r*1.5);  
     },
-    drawStatusPoint(color){//转换初始圆的颜色形成并启动画小圆函数   
+    drawStatusPoint(color){//@Params颜色字符串转换初始圆的颜色形成并启动画小圆函数   
         for (let index of this.record) {
             this.ctx.strokeStyle = color;
-            this.ctx.lineWidth = 2;
+            this.ctx.lineWidth = 3;
             this.ctx.beginPath();
             this.ctx.arc(this.arr[index].x, this.arr[index].y, this.r, 0, Math.PI * 2);
             this.ctx.stroke();
-            this.drawPicture(index)
+            this.drawContainer(index)
         }  
     },
-    drawLine(begin, end){//在两个点之间连线
+    drawLine(begin, end){//@Params起始点和重点坐标对象  在两个点之间连线
         this.ctx.beginPath();
         this.ctx.moveTo(begin.x, begin.y)
         this.ctx.lineTo(end.x, end.y)
@@ -112,20 +121,19 @@ Object.assign(touchLock.prototype, {
         this.ctx.clearRect(0, 0, this.width, this.height)
         this.drawCircle()
     },
-    //测试点是否在解锁点内     4\5让动画更自然一些？？
-    testPosition(x, y){
+    testPosition(x, y){//@Params鼠标坐标值  测试点是否在解锁点内   4\5让动画更自然一些？？
         for(let index of this.arr.keys()){
             if (Math.abs(x - this.arr[index].x) < this.r && Math.abs(y - this.arr[index].y) < this.r) {
                 return index
             }
         }
     },
-    updata(x, y){//每次touchmove渲染画布内容包括小圆以及连线
+    updata(x, y){//@Params当前鼠标坐标数值  每次touchmove渲染画布内容包括小圆以及连线
         let lastcircle;
 
         for(let index of this.record){
             if (this.arr[index]) {
-                this.drawPicture(index)
+                this.drawContainer(index)
 
                 if (lastcircle) {
                     this.drawLine(lastcircle, this.arr[index])
@@ -142,7 +150,7 @@ Object.assign(touchLock.prototype, {
     setPassword(){
         if(this.testStep === 1) {
             if (this.record.length !== this.tempRecord.length) {
-                title.innerHTML = '两次输入不一致,请重新输入'
+                title.innerHTML = '输入不一致,请重新输入'
                 this.drawStatusPoint('#fb4920')
                 this.testStep = 0
             }else{
@@ -158,7 +166,7 @@ Object.assign(touchLock.prototype, {
                     this.testStep = 2
                     window.localStorage.handPassword = this.tempRecord.join(' ')
                 }else{
-                    title.innerHTML = '两次输入不一致,请重新输入'
+                    title.innerHTML = '输入不一致,请重新输入'
                     this.drawStatusPoint('#fb4920')
                     this.testStep = 0
                     this.tempRecord = []
@@ -222,35 +230,47 @@ Object.assign(touchLock.prototype, {
         this.canvas.addEventListener('touchstart', function(e){//测试当前位置数据并进行处理   触发频率高，所以不需要start时验证
             e.preventDefault();
         })
-        this.canvas.addEventListener('touchmove', function(e){//测试当前位置数据并进行处理
-            let rect = e.currentTarget.getBoundingClientRect(),
-                x = e.touches[0].clientX - rect.left,
-                y = e.touches[0].clientY - rect.top,
-                res = _this.testPosition(x, y),
-                flag = true;
 
-            if(typeof res === 'number'){
-                for(let i of _this.record){
-                    if (i === res) {
-                        flag = false
+        this.canvas.addEventListener('touchmove', function(e){//测试当前位置数据并进行处理
+            if (_this.timer) {
+                let rect = e.currentTarget.getBoundingClientRect(),
+                    x = e.touches[0].clientX - rect.left,
+                    y = e.touches[0].clientY - rect.top,
+                    res = _this.testPosition(x, y),
+                    flag = true;
+
+                if(typeof res === 'number'){
+                    for(let i of _this.record){
+                        if (i === res) {
+                            flag = false
+                        }
+                    }
+                    if (flag) {
+                        _this.lastPoint = res
+                        _this.record.push(res)
+                        _this.drawContainer(_this.lastPoint)
                     }
                 }
-                if (flag) {
-                    _this.lastPoint = res
-                    _this.record.push(res)
-                    _this.drawPicture(_this.lastPoint)
-                }
-            }
 
-            _this.resetDraw()
-            _this.updata(x, y)
+                _this.resetDraw()
+                _this.updata(x, y)
+                _this.timer = false
+
+                setTimeout(function(){
+                    _this.timer = true
+                },40)
+            }else{
+                return 
+            }
         })
+
         this.canvas.addEventListener('touchend', function(e){
             _this.dealPassword()
             _this.record = []
             _this.lastPoint = undefined
             _this.endCallback && _this.endCallback()
-            _this.initYellowMan()
+            _this.computeYellowMan()
+
             setTimeout(function(){
                 _this.resetDraw()
             }, 300);
